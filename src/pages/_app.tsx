a42766100f64c8da29917b8206f890c19d99ebc3
Jang Haemin <io@jhaemin.com>
2020-04-19 16:48:15 +0900

    config: run linting and type checking before commit - Fix imports order

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import App from 'next/app'
-import Head from 'next/head'
 // Include globally common styles inside `globalstyle.scss`
 // and import it only once
 import '@/styles/global/globalstyle.scss'
+
+import App from 'next/app'
+import Head from 'next/head'
+import React from 'react'
 
 export default class MyApp extends App {
   public render(): JSX.Element {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import { NextPage } from 'next'
 import Main from '@/components/Main'
+import { NextPage } from 'next'
 
 const Page: NextPage = () => {
   return <Main />
