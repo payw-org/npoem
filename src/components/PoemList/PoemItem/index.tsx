3719ef06c6d0eb345c1b4d22f5cb313dabf79069
Choibumsu <bumsu0211@gmail.com>
2020-04-28 00:18:47 +0900

    [NPOEM-12] Make PoemItem structure using example data

--- src/components/PoemList/PoemItem/index.tsx
+++ src/components/PoemList/PoemItem/index.tsx
@@ -1,9 +1,26 @@
 import './style.scss'
 
-type PoemItemProps = {}
+type PoemItemProps = {
+  username: string
+  word: string
+  poem: string[]
+}
 
-const PoemItem: React.FC<PoemItemProps> = () => {
-  return <div className="component-poem-item">Poem Item</div>
+const PoemItem: React.FC<PoemItemProps> = ({ username, word, poem }) => {
+  return (
+    <div className="component-poem-item">
+      <div>{username}</div>
+      {word.split('').map((letter, index) => {
+        return (
+          <div key={index}>
+            <div>{letter}</div>
+            <div>{poem[index]}</div>
+          </div>
+        )
+      })}
+      <hr />
+    </div>
+  )
 }
 
 export default PoemItem
--- src/components/PoemList/index.tsx
+++ src/components/PoemList/index.tsx
@@ -4,12 +4,36 @@ import PoemItem from '@/components/PoemList/PoemItem'
 
 type PoemListProps = {}
 
+type exType = {
+  id: number
+  username: string
+  word: string
+  poem: string[]
+}
+
+const examples: exType[] = [
+  {
+    id: 1,
+    username: '김정빈지노',
+    word: '홍길동',
+    poem: ['홍대', '길바닥에서', '동동주 한잔'],
+  },
+  {
+    id: 2,
+    username: '장해민초의난',
+    word: '아이돌',
+    poem: ['아저씨', '이제 자리로 좀', '돌아가주세요 안보여요'],
+  },
+]
+
 const PoemList: React.FC<PoemListProps> = () => {
   return (
     <div className="component-poem-list">
-      <PoemItem />
-      <PoemItem />
-      <PoemItem />
+      <div>실시간 엔행시</div>
+      <hr />
+      {examples.map((example) => {
+        return <PoemItem key={example.id} {...example}></PoemItem>
+      })}
     </div>
   )
 }
